239bdf8647fb4bb785561072323ae05bf9e7f0a4
neepix <neepa.sini@gmail.com>
2024-07-26 19:04:29 +0200

    fixed missed const

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,5 @@
-'use client';
- 
+'use client'
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -8,12 +8,21 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
- 
-// ...
- 
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
+
 export default function NavLinks() {
   const pathname = usePathname();
- 
   return (
     <>
       {links.map((link) => {
@@ -27,8 +36,7 @@ export default function NavLinks() {
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
-            )}
-          >
+            )}          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
